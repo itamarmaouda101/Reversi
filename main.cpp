443db115d014df159b3c24ba8288b8215013c253
itamarmaouda101 <itamarmaouda101@gmail.com>
2020-11-21 09:57:19 +0200

    add game fichers

--- main.cpp
+++ main.cpp
@@ -5,11 +5,20 @@ class Game
 {
     private:
         int player_won = 0;
-    public: 
-        
-        int plays_now = 0;
         int board[8][8];
-        void clear_board()
+        int plays_now = 0;
+    public:
+    //board opertions 
+    ///###############
+        void set_board(int x, int y , int player_number)
+        {
+            board[x][y] = player_number;
+        }
+        int get_board(int x, int y)
+        {
+            return board[x][y];
+        }
+        void clear_board(int board[8][8])
         {
             for(int i=0; i<8;i++)
             {
@@ -19,7 +28,7 @@ class Game
                 }
             }
         }
-        void show_board()
+        void show_board_all_disk(int board[8][8])
         {
             for(int i=0; i<8;i++)
             {
@@ -30,6 +39,20 @@ class Game
                 }
             }
         }
+        
+        //###################
+        
+        
+        //player opertions
+        //######
+        int get_plays_now()
+        {
+            return plays_now;
+        }
+        void add_player()
+        {
+            plays_now++;
+        }
         void set_player_won(int player)
         {
             player_won = player;
@@ -38,6 +61,11 @@ class Game
         {
             return player_won;
         }
+        //######
+
+        //game opertions
+        //#######
+        int add_disk(int x, int y);
         int place_is_free(int x, int y)
         {
             if (board[x][y] == 0)
@@ -99,52 +127,167 @@ class Game
             }
             return 0;
         }
+        //#####
+        
 };
 class Player: public Game
 {
+    protected:
+        int board_ops[8][8];
     public:
         int player_number;
         int number_of_disks_on_table = 0;
         int number_of_disks_left;
+        
+        //board_ops getter and setter
+        //###
+        void set_board_ops(int x,int y, int set_code)
+        {
+            board_ops[x][y] == set_code;
+        }
+        int get_board_ops(int x, int y)
+        {
+            return board_ops[x][y];
+        }
+        //###
         int finish_my_disks()
         {
             return number_of_disks_left == 0;
         }
+        void mark_ops_board(int x, int y)
+        {
+            if (((x == 0 || y == 0) || (x == 7 || y == 7)) )
+            {
+                
+                if (x == 0 || y == 0)
+                {
+                    
+                    if(get_board(x,y+1) == player_number)
+                    {
+                        set_board_ops(x,y+1, player_number);
+                        mark_ops_board(x,y+1);
+                    }
+                    if(get_board_ops(x+1, y+1) == player_number)
+                    {
+                        set_board_ops(x+1,y+1, player_number);
+                        mark_ops_board(x+1,y+1);
+                    }
+                    if(get_board_ops(x+1,y) == player_number)
+                    {
+                       set_board_ops(x+1,y, player_number);
+                       mark_ops_board(x+1,y); 
+                    }
+                }
+                if(x == 7 || y == 7)
+                //3ops
+                if(x == 0 || 0<y<7)
+                {
+                    for(int i=y-1; i<y+1;i++)
+                    {
+                        if(get_board(i, ))
+                    }
+                }
+                //5 ops
+                if(y == 0 || 0<x<7)
+                //5 ops
+                if()
+            }
+            //hundle as the hole in the middle
+            for (int i=x; i<x+3;i++)
+            {
+                //checks if there is anoter player's disks
+                //#######
+                if (get_board(i,y+1) == player_number)
+                {
+                    set_board_ops(i, y+1, player_number);
+                    mark_ops_board(i , y+1);
+                    //handle as found place to add search
+                }
+                if(get_board(i, y) == player_number || i != x)
+                {
+                    set_board_ops(i, y, player_number);
+                    mark_ops_board(i , y);
+                }
+                    //handle as found place to add search
+                if (get_board(i,y-1) == player_number)
+                {
+                    set_board_ops(i, y-1, player_number);
+                    mark_ops_board(i , y-1);
+                 
+                    //handle as found place to add search 
+                }
+                //######
+
+
+                //check's if the place is free
+                //#####
+                // i code that hole with:
+                // 6 is empty hole
+                // 0 is enemy hole
+                // player_number is the player hole
+                if (get_board(i,y+1) == 0)
+                    set_board_ops(i, y+1, 6);
+                    //handle as found place to add disk
+                if(get_board(i, y) == 0 )
+                    set_board_ops(i, y, 6);
+                    //handle as found place to add disk
+                if (get_board(i,y-1) == 0)
+                    set_board_ops(i, y-1, 6);
+                    //handle as found place to add disk        
+            }
+
+        }
+        void set_player_opsions()
+        {
+            for(int i=0;i<8;i++)
+            {
+                for(int j=0;j<8;j++)
+                {
+                   mark_ops_board(i,j); 
+                }
+            }
+            
+        }
+        int play()
+        {
+            clear_board(board_ops);
+            set_player_opsions();
+            cout << "show the board\n";
+            show_board_all_disk(board_ops);
+            int x, y, ret = -1;
+            cout << "enter x place\n";
+            cin >>x;
+            cout << "enter y place\n";
+            cin >>y;
+            while (ret != 1)
+            {
+                ret = add_disk(x, y);
+            }
+            if (finish_my_disks())
+                set_player_won(player_number);
+                return 5;
+        }
         int add_disk(int x, int y)
         {
            
-            if (place_is_free(x, y) == player_number)
+            if (place_is_free(x, y) != 0)
                 return -1;
                 //when player is want to put disk where hw already have one
-            else if (place_is_free(x, y) == 0)
-            {
-                //hundle as put the disk
-                board[x][y] = player_number;
-                number_of_disks_on_table ++;
-                number_of_disks_left--;
-                if (finish_my_disks())
-                    set_player_won(player_number);
-                    return 5; // 5 means winning!
-                return 1;
-            }
             else
             {
-                //hunde as flip the disk
-                board[x][y] = player_number;
-                 if (finish_my_disks())
-                    set_player_won(player_number);
-                    return 5; // 5 means winning! 
-                return 2;
-            }
-            
-            
+                //hundle as put the disk
+                set_board(x, y, player_number);
+                number_of_disks_on_table ++;
+                number_of_disks_left--;
+                return 1;
+            }     
         }
         Player(int number_disks)
         {
-            player_number = plays_now + 1;
+            player_number = get_plays_now() + 1;
             number_of_disks_left = number_disks;
             number_of_disks_on_table = 0;
-            plays_now++;
+            add_player();
         }
 };
 
